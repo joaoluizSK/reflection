964cd05b57347a7ec7a759a098608fe444536a10
JoaoLuizSK <JoaoLuizSK@mbp-de-joao>
2016-08-11 23:16:28 -0300

    Examples for the second chapter.

---
+++ Example01/src/chapter2/references/ReferenciaEstaticaClasse.java
@@ -0,0 +1,14 @@
+package chapter2.references;
+
+public class ReferenciaEstaticaClasse {
+
+	public static void main(String[] args) {
+		Class<String> classe = String.class;
+		System.out.println(classe.getName());
+		imprimeNomeClasse(Integer.class);
+	}
+	
+	public static void imprimeNomeClasse(Class<?> classe){
+		System.out.println("Chamado o método com " + classe.getName());
+	}
+}
